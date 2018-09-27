44d6673d730509128cb14080184580c940aa6b75
edz <edz@edzdeMacBook-Pro.local>
2018-09-27 12:14:55 +0800

    初始化工程

---
+++ build/webpack.demo.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const merge = require('webpack-merge')
+const webpackDevConfig = require('./webpack.dev')
+const config = require('./config')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+
+
+const webpackDemoConfig = merge(webpackDevConfig, {
+    entry: path.resolve(config.srcPath, 'index.js'),
+    output: {
+        filename: 'index.js',
+        path: path.resolve(config.basePath, './doc'),
+        publicPath: '/'
+    },
+
+    module: {
+        rules: [{
+            test: /\.html$/,
+            use: [{loader: 'html-loader'}]
+        }, ]
+    }, 
+    
+    plugins: [
+        new htmlWebpackPlugin({
+            template: path.join(config.srcPath, 'index.html'),
+            inject: 'body'
+        })
+    ]
+})
+
+
+module.exports = webpackDemoConfig
