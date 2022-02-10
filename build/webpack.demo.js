39565a71f1a6d9d020b014ebaa08b6c605ab479d
zhoupeng <zhoupeng1995@live.com>
2022-02-10 10:20:01 +0800

    给简历添加二维码

--- build/webpack.demo.js
+++ build/webpack.demo.js
@@ -4,6 +4,7 @@ const webpackDevConfig = require('./webpack.dev')
 const config = require('./config')
 const htmlWebpackPlugin = require('html-webpack-plugin')
 const WebpackMarkdownToPdfPlugin = require('webpack-markdown-pdf-plugin')
+const through2 = require('through2')
 
 
 const webpackDemoConfig = merge(webpackDevConfig, {
@@ -28,6 +29,23 @@ const webpackDemoConfig = merge(webpackDevConfig, {
         }),
         new WebpackMarkdownToPdfPlugin({
             filename: './pdf/[name].pdf',
+            paperBorder: '0.8cm',
+            preProcessHtml: function() {
+                let isBegin = true
+                return through2({ objectMode: true, allowHalfOpen: false }, function (chunk, enc, callback) {
+                    if (isBegin) {
+                        const prefix = Buffer.from('<div class="markdown-body"><img style="position: absolute; right: 20px; top: 20px; width: 80px;" src="https://user-images.githubusercontent.com/23492006/153324006-1ccb9b52-4fb5-4d80-95d0-b2f56617965f.png" />', 'UTF-8')
+                        this.push(Buffer.concat([prefix, chunk]))
+                        isBegin = false
+                    } else {
+                        this.push(chunk)
+                    }
+                    callback()
+                }, function(cb) {
+                    this.push(Buffer.from('</div>', 'UTF-8'));
+                    cb()
+                })
+            }
         })
     ]
 })
